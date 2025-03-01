b55683d2bb01dadf1a4d2341763ff3291f15d104
mibhumi <bhumipatel1196@gmail.com>
2025-03-01 17:16:53 -0500

    setting up express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 console.log("starting a new project");
+
+const express = require("express");
+
+const app = express();
+
+// request handler
+app.use("/test", (req,res) => {
+    res.send("Hello from the server");
+});
+
+app.listen(3001, ()=>{console.log("Server is listening on port number 3001");});
